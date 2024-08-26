a2edcc562f09f7b416847cfdbf0328d9648fca5e
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-08-26 13:18:47 -0400

    Create session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:user][:name])
+
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:alert] = "Invalid credentials"
+      redirect_to session_path
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to session_path
+  end
+end
